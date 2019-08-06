921a85182bdce7b8bbe24efc087680c629b0f1d6
Andy Parinas <andy.parinas@gmail.com>
2019-08-06 22:02:01 +1000

    Setup the test class and methods

---
+++ tests/Feature/ProjectsTest.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace Tests\Feature;
+
+use Tests\TestCase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+
+class ProjectsTest extends TestCase
+{
+    use WithFaker, RefreshDatabase;
+
+
+    /** @test */
+    public function a_user_can_create_a_project()
+    {
+
+        /**
+         * will disable Laravel graceful error handling.
+         * Good to disable in the Test Class
+         */
+        $this->withoutExceptionHandling();
+
+        $attributes = [
+            'title' => $this->faker->sentence,
+            'description' => $this->faker->paragraph
+        ];
+
+        $this->post('/projects', $attributes)->assertRedirect('/projects');
+
+        $this->assertDatabaseHas('projects', $attributes);
+
+        $this->get('/projects')->assertSee($attributes['title']);
+
+    }
+}
