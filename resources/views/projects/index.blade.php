753995bd74843d1fc54d892648062c346ae21235
Andy Parinas <andy.parinas@gmail.com>
2019-08-07 15:21:05 +1000

    configure Project index initial design

--- resources/views/projects/index.blade.php
+++ resources/views/projects/index.blade.php
@@ -2,16 +2,16 @@
 
 @section('content')
 
-    <div class="container">
+    <div class="container mx-auto">
         <div class="flex items-center">
-            <h1 class="mr-auto">Birdboard</h1>
+            <h1 class="mr-auto">Project List</h1>
             <a href="/projects/create">New Project</a>
         </div>
-        <div class="flex">
+        <div class="flex pt-4">
             @forelse ($projects as $project)
-                <div class="bg-white mr-4 rounded shadow">
-                    <h3>{{ $project->title }}</h3>
-                    <div> {{ $project->description }} </div>
+                <div class="bg-white mr-4 rounded shadow w-1/3 p-5 h-20" style="height:200px;">
+                    <h2 class="font-normal text-xl mb-4 py-2">{{ $project->title }}</h2>
+                    <div class="text-gray-600"> {{str_limit($project->description,150) }} </div>
                 </div>
 
             @empty
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,25 @@
 module.exports = {
   theme: {
-    extend: {}
+    shadows: {
+      default: '0 0 5px 0 rgba(0,0,0,0.08)'
+    },
+    extend: {
+      colors: {
+        gray: {
+          50: '#F5F6F9',
+          75: 'rgba(0,0,0,0.4)',
+          100: '#f7fafc',
+          200: '#edf2f7',
+          300: '#e2e8f0',
+          400: '#cbd5e0',
+          500: '#a0aec0',
+          600: '#718096',
+          700: '#4a5568',
+          800: '#2d3748',
+          900: '#1a202c',
+        }
+      },
+    }
   },
   variants: {},
   plugins: []
