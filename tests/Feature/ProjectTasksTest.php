97c1ec36f554e8a968f72158c79cec77dea67230
Andy Parinas <andy.parinas@gmail.com>
2019-08-10 17:51:38 +1000

    refactor test to reduced repetition

--- tests/Feature/ProjectTasksTest.php
+++ tests/Feature/ProjectTasksTest.php
@@ -6,6 +6,7 @@ use Tests\TestCase;
 use Illuminate\Foundation\Testing\WithFaker;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use App\Project;
+use Tests\Setup\ProjectFactory;
 
 class ProjectTasksTest extends TestCase
 {
@@ -14,15 +15,15 @@ class ProjectTasksTest extends TestCase
 
     /** @test */
     public function project_can_have_tasks()
-    {
-        $this->withoutExceptionHandling();
+    {  
+        // $this->signIn();
 
-        
-        $this->signIn();
+        // $project = factory(Project::class)->create(['owner_id' => auth()->id()]);
 
-        $project = factory(Project::class)->create(['owner_id' => auth()->id()]);
+        $project = app(ProjectFactory::class)->create();
 
-        $this->post($project->path() . "/tasks", ['body' => 'Test Task']);
+        $this->actingAs($project->owner)
+            ->post($project->path() . "/tasks", ['body' => 'Test Task']);
 
         $this->get($project->path())
             ->assertSee('Test Task');
@@ -50,34 +51,60 @@ class ProjectTasksTest extends TestCase
     /** @test */
     public function task_requires_a_body()
     {
-        $this->signIn();
+        // $this->signIn();
 
-        $project = auth()->user()->projects()->create(
-            factory(Project::class)->raw()
-        );
+        // $project = auth()->user()->projects()->create(
+        //     factory(Project::class)->raw()
+        // );
 
+        $project = app(ProjectFactory::class)->create();
 
         $attributes = factory('App\Task')->raw(['body'=>'']);
 
 
-        $this->post($project->path() . '/tasks', $attributes)->assertSessionHasErrors('body');
+        $this->actingAs($project->owner)
+            ->post($project->path() . '/tasks', $attributes)->assertSessionHasErrors('body');
     }
 
     /** @test */
     public function a_task_can_be_updated()
     {
-        $this->signIn();
+        // $this->signIn();
 
-        $project = auth()->user()->projects()->create(
-            factory(Project::class)->raw()
-        );
+        // $project = auth()->user()->projects()->create(
+        //     factory(Project::class)->raw()
+        // );
 
-        $task = $project->addTask('test task');
+        // $task = $project->addTask('test task');
 
-        $this->patch($task->path(), [
-            'body' => 'Changed',
-            'completed' => true
-        ]);
+
+        /**
+         * A refactor of the above code using 
+         * the setup class defined in tests\Setup
+         */
+        // $project = app(ProjectFactory::class)
+        // ->ownedBy($this->signIn())
+        // ->withTasks(1)->create();
+     
+        // $this->patch($project->tasks[0]->path(), [
+        //     'body' => 'Changed',
+        //     'completed' => true
+        // ]);
+
+        /**
+         * The above code, we explicitly tell the ProjectFactory setup class to signing a user for us
+         * The below code is a refactor where we use the created user inside the ProjectFactory
+         * to use that as authenticating user
+         */
+
+        $project = app(ProjectFactory::class)
+            ->withTasks(1)->create();
+
+        $this->actingAs($project->owner)
+            ->patch($project->tasks[0]->path(), [
+                    'body' => 'Changed',
+                    'completed' => true
+                ]);
 
         $this->assertDatabaseHas('tasks', [
             'body' => 'Changed',
@@ -91,11 +118,14 @@ public function only_owner_of_project_can_update_task()
     {
         $this->signIn();
 
-        $project = factory(Project::class)->create();
+        // $project = factory(Project::class)->create();
 
-        $task = $project->addTask('test task');
+        // $task = $project->addTask('test task');
 
-        $this->patch($task->path(), [
+        $project = app(ProjectFactory::class)
+        ->withTasks(1)->create();
+
+        $this->patch($project->tasks[0]->path(), [
             'body' => 'Changed'
         ])->assertStatus(403);
 
